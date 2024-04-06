c4c2bd2d8e799bba3073fff726431abf4042fba0
DivanoRizqiA <divanorizqianandra@student.uir.ac.id>
2024-04-06 19:39:05 +0700

    vue-tailwind-portfolio-main

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center my-4 dark:text-white flex items-center justify-center">
+            Berkuliah di Universitas Islam Riau <a href="https://github.com/RifaAlyaS" target="_blank"
+                class="text-white py-1 px-2 ms-2 rounded-md bg-blue-600">@RifaAlyaS</a>
+        </div>
+        <div class="text-center">
+            <a href="" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-linkedin" /></a>
+            <a href="" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-facebook" /></a>
+            <a href="" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-instagram" /></a>
+            <a href="c" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-brands fa-github" /></a>
+            <a href=""
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-solid fa-envelope" /></a>
+        </div>
+    </div>
+</template>
+<script setup></script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="../assets/imgs/Foto.jpg"
+            class="w-[200px] h-[200px] sm:w-[120px] sm:h-[200px] mx-auto rounded-full shadow-md">
+        <h1 class="mt-8 mb-4 text-center text-3xl sm:text-4xl uppercase font-bold dark:text-white">Rifa Alya Shadrina</h1>
+        <hr class="border-1 my-4 border-gray-400 w-14 mx-auto ">
+        <p class="text-center text-xl sm:text-2xl text-gray-700 dark:text-gray-300">Developer</p>
+    </div>
+</template>
+
+<script setup></script>
